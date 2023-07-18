b7b697b26ada678cc9b0e78d09b729d07e95f4ce
Rbr4t <robertkoor0@gmail.com>
2023-07-18 19:19:44 +0300

    setup tests + wrote tests for Item component

---
+++ tests/Item.test.jsx
@@ -0,0 +1,64 @@
+import { render, screen, waitFor } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import Item from "../src/components/Item";
+import { vi } from "vitest";
+
+import { handleClick } from '../src/components/Item' 
+
+describe('Visual appearance', () => {
+    
+    it('has title and price tag', () => {
+        render(<Item name={'test lamp'} id={1} price={100}/>)
+        expect(screen.getByRole('heading', {level: 2}).textContent).toMatch(/test lamp/i)
+        expect(screen.getByRole('heading', {level: 4}).textContent).toMatch(/100/i)
+    });
+
+    it('has a "Add to cart" button', () => {
+        render(<Item name={'test lamp'} id={1} price={100}/>)
+        expect(screen.getByRole('button', {name: 'Add to cart'}))
+    });
+
+})
+
+describe("Functional parts of the component", () => {
+    it('can increase the quantity', async () => {
+        const user = userEvent.setup()
+        const item = {
+            name: 'Test Item',
+            id: 123,
+            price: 9.99,
+        };
+        
+
+        render( <Item name={item.name} id={item.id} price={item.price} />)
+        const button = screen.getByRole('button', {name: '+'})
+        await user.click(button)
+        
+        await waitFor(() => {
+            const quantityElement = screen.getByText(/Quantity:/);
+            expect(quantityElement).toHaveTextContent('Quantity: 2');
+          });
+
+    })
+
+    it('can decrease the quantity', async () => {
+        const user = userEvent.setup()
+        const item = {
+            name: 'Test Item',
+            id: 123,
+            price: 9.99,
+        };
+        
+        render( <Item name={item.name} id={item.id} price={item.price} />)
+        const button = screen.getByRole('button', {name: '-'})
+        await user.click(button)
+        
+        await waitFor(() => {
+            const quantityElement = screen.getByText(/Quantity:/);
+            expect(quantityElement).toHaveTextContent('Quantity: 1');
+          });
+
+    });
+
+    it("can add a new order", () => {});
+})
