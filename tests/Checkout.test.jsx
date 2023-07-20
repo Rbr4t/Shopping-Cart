2b7cabc4299226f30302cb793e453d56b3a3b894
Rbr4t <robertkoor0@gmail.com>
2023-07-20 20:29:16 +0300

    tests for checkout logic

--- tests/Checkout.test.jsx
+++ tests/Checkout.test.jsx
@@ -28,6 +28,67 @@ describe("Component renders", () => {
     });
 })
 
+const localStorageFunc = () => {
+    const data = [{"id":1,"name":"table lamp","price":100,"quantity":3},{"id":3,"name":"salt lamp","price":250,"quantity":1},{"id":2,"name":"night lamp","price":200,"quantity":1}]
+    localStorage.setItem('orders', JSON.stringify(data));
+}
+
 describe('functional parts', () => {
-    it()
+    beforeAll(localStorageFunc)
+    it("add additional item", async () => {
+        const user = userEvent.setup()
+        
+        render( 
+            <MemoryRouter>
+                <Checkout />
+            </MemoryRouter>
+        )
+        const button = screen.getAllByRole('button', {name: '+'})[0]
+        await user.click(button)
+        screen.debug()
+        expect(screen.getByRole('table')).toContainHTML('<td>4</td>')
+        
+    });
+
+    it("remove additional item", async () => {
+        const user = userEvent.setup()
+        
+        render( 
+            <MemoryRouter>
+                <Checkout />
+            </MemoryRouter>
+        )
+        const button = screen.getAllByRole('button', {name: '-'})[0]
+        await user.click(button)
+        screen.debug()
+        expect(screen.getByRole('table')).toContainHTML('<td>2</td>')
+        
+    });
+
+    it("trash an item", async () => {
+        const user = userEvent.setup()
+        
+        render( 
+            <MemoryRouter>
+                <Checkout />
+            </MemoryRouter>
+        )
+        const button = screen.getAllByRole('button', {name: 'trash'})[0]
+        await user.click(button)
+        expect(screen.getAllByRole('row').length).toEqual(3)
+        
+    });
+
+    it("shows cumulative sum", () => {
+        const data = [{"id":1,"name":"table lamp","price":100,"quantity":3},{"id":3,"name":"salt lamp","price":250,"quantity":1}]
+        localStorage.setItem('orders', JSON.stringify(data));
+
+        render( 
+        <MemoryRouter>
+            <Checkout />
+        </MemoryRouter>
+        )
+        
+        expect(screen.getAllByRole('button')[screen.getAllByRole('button').length-1].textContent).toContain('550')
+    });
 })
